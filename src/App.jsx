90914628edbb05d1c2e9edad6b22d538b9040d24
waheedSALAH <w55h44@gmail.com>
2025-04-28 16:00:12 +0300

    Add homepage and deployment scripts; update routing and Vite config for GitHub Pages deployment
    - Added "homepage" field to package.json for GitHub Pages.
    - Introduced "deploy" and "predeploy" scripts in package.json for easier deployment.
    - Updated App.jsx to set the Router basename for proper routing on GitHub Pages.
    - Configured Vite to use the correct base path for deployment.
    - Added "gh-pages" dependency to package.json for deployment functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,13 @@ import { Register } from './pages/register';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { TicketDetails } from './pages/TicketDetails';
 import MyTicket from './pages/myTicket';
-import {AdminPanel} from './pages/admin';
+import { AdminPanel } from './pages/admin';
 import { AddTrain } from './pages/AddTrain';
 import { EditTrain } from './pages/EditTrain';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Train_Ticket">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -40,8 +40,6 @@ function App() {
         <Route path="/EditTrain/:id" element={<EditTrain />} />
         <Route path="/ticket-details/:id" element={<TicketDetails />} /> 
         <Route path="/ticket-details/" element={<MyTicket />} /> 
-        
-
       </Routes>
     </Router>
   );
