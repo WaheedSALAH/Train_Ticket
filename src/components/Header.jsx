e18f0f7e8c086241dd4b3caf7ad466457534fbc3
وحيد صلاح عيداروس سالم <168032127+WaheedSALAH@users.noreply.github.com>
2025-06-19 07:28:17 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,20 @@ export function Header() {
     return (
         <Navbar expand="lg" bg="dark" variant="dark" className="shadow-lg">
             <Container>
-                <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
-  <img
-    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQUmUDn9WZD5uXSW9FNekZ4iigNP7PQaFAvA&s"
-    alt="Logo"
-    style={{ width: "80px", height: "50px", objectFit: "cover" }}
-    className="rounded me-2"
-  />
-  <span className="text-warning fw-bold fs-3 shadow-sm">
-    Train Ticket
-  </span>
+<Navbar.Brand className="d-flex align-items-center">
+  <NavLink to="/" className="d-flex align-items-center text-decoration-none">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQUmUDn9WZD5uXSW9FNekZ4iigNP7PQaFAvA&s"
+      alt="Logo"
+      style={{ width: "80px", height: "50px", objectFit: "cover" }}
+      className="rounded me-2"
+    />
+    <span className="text-warning fw-bold fs-3 shadow-sm">
+      Train Ticket
+    </span>
+  </NavLink>
 </Navbar.Brand>
+
 
 
                 <Navbar.Toggle aria-controls="navbar-nav" />
